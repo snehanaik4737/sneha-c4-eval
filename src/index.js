082419f131aeb07cfc4d742f2ae201a5e77ddf6d
snehanaik4737 <97469677+snehanaik4737@users.noreply.github.com>
2022-03-28 11:19:09 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const express =require("express");
 
-
-const usersController = require("./controllers/user.controller");
-const todoController = require("./controllers/todo.controller");
-
-const {register,login}=require("./controllers/auth.controller")
-
 const app = express();
 
 app.use(express.json());
+
+
+const {register,login}=require("./controllers/auth.controller")
+const usersController = require("./controllers/user.controller");
+const todoController = require("./controllers/todo.controller");
+
+
 
 app.use("/users", usersController);
 
